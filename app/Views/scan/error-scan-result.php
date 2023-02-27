f63dc93a913907c37303575803f5b8723e356ebb
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-02-27 16:05:54 +0700

    Update scan result view
    siswa
    error

--- app/Views/scan/error-scan-result.php
+++ app/Views/scan/error-scan-result.php
@@ -1,1 +1,6 @@
 <h3 class="text-danger"><?= $msg; ?></h3>
+<?php if ($data != NULL) : ?>
+   <?php foreach ($data as $key => $value) : ?>
+      <p><?= $key; ?><b><?= $value; ?></b></p>
+   <?php endforeach ?>
+<?php endif ?>
--- app/Views/scan/scan-result-card-siswa.php
+++ app/Views/scan/scan-result-card-siswa.php
@@ -1,4 +1,5 @@
 <h3 class="text-success">Absen berhasil</h3>
 <p><b>Nama : </b><?= $data['nama_siswa']; ?></p>
+<p><b>NIS : </b><?= $data['nis']; ?></p>
 <p><b>Kelas : </b><?= $data['kelas']; ?></p>
 <p><b>Jurusan : </b><?= $data['jurusan']; ?></p>
