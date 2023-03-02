3b964c8b62ef6e6947233643c8b428b97dad51e8
RPL Cinta Niaga <93766033+x4nn@users.noreply.github.com>
2023-03-02 08:07:41 +0700

    Fix ui

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -1,12 +1,12 @@
-<div class="sidebar" data-color="azure" data-background-color="white" data-image="<?= base_url('public/assets/img/sidebar-1.jpg'); ?>">
+<div class="sidebar" data-color="azure" data-background-color="white" data-image="<?= base_url('public/assets/img/sidebar/sidebar-1.jpg'); ?>">
    <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
         Tip 2: you can also add an image using data-image tag
     -->
    <div class="logo">
-      <a href="" class="simple-text logo-normal">
-         Absensi ICBCN
+      <a class="simple-text logo-normal">
+         Operator<br>Petugas Absensi
       </a>
    </div>
    <div class="sidebar-wrapper">
