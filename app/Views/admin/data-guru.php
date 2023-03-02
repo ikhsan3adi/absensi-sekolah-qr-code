018cb8639a72feb5b362a993d8453d8b44c891d6
RPL Cinta Niaga <93766033+x4nn@users.noreply.github.com>
2023-03-02 08:07:13 +0700

    Update data-guru.php
    Show guru list

--- app/Views/admin/data-guru.php
+++ app/Views/admin/data-guru.php
@@ -2,6 +2,32 @@
 <?= $this->section('content') ?>
 <div class="content">
     <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-success">
+                        <h4 class="card-title">Daftar Guru</h4>
+                        <p class="card-category">Angkatan 2022/2023</p>
+                    </div>
+                    <div class="card-body table-responsive">
+                        <table class="table table-hover">
+                            <thead class="text-success">
+                                <th>ID</th>
+                                <th>Nama Guru</th>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($data as $value) : ?>
+                                    <tr>
+                                        <td><?= $value['id_guru']; ?></td>
+                                        <td><?= $value['nama_guru']; ?></td>
+                                    </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <?= $this->endSection() ?>
