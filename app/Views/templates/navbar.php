6ee0d5031613b7c3e08cda8b165af9b0e49a1a28
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-05 22:19:33 +0700

    Add scan button to admin navbar

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
     <div class="container-fluid">
         <div class="navbar-wrapper">
-            <p class="navbar-brand"><?= $title; ?></p>
+            <p class="navbar-brand"><b><?= $title; ?></b></p>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -25,25 +25,17 @@
                     <a class="nav-link" href="<?= base_url('admin/dashboard'); ?>">
                         <i class="material-icons">dashboard</i>
                         <p class="d-lg-none d-md-block">
-                            Stats
+                            Dashboard
                         </p>
                     </a>
                 </li>
-                <li class="nav-item dropdown">
-                    <!-- <a class="nav-link" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="material-icons">notifications</i>
-                        <span class="notification">5</span>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('scan/masuk'); ?>">
+                        <i class="material-icons">qr_code</i>
                         <p class="d-lg-none d-md-block">
-                            Some Actions
+                            Scan
                         </p>
-                    </a> -->
-                    <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Mike John responded to your email</a>
-                        <a class="dropdown-item" href="#">You have 5 new tasks</a>
-                        <a class="dropdown-item" href="#">You're now friend with Andrew</a>
-                        <a class="dropdown-item" href="#">Another Notification</a>
-                        <a class="dropdown-item" href="#">Another One</a>
-                    </div> -->
+                    </a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
