8449e77c302e6e26b4547f311cf043a810ede23e
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-08 00:32:44 +0700

    Added nis on list data siswa
    UI improve

--- app/Views/admin/data/list-data-siswa.php
+++ app/Views/admin/data/list-data-siswa.php
@@ -1,24 +1,36 @@
 <div class="card-body table-responsive">
-    <table class="table table-hover">
-        <thead class="text-primary">
-            <th>ID</th>
+   <?php if (!$empty) : ?>
+      <table class="table table-hover">
+         <thead class="text-success">
+            <th>No</th>
+            <th>NIS</th>
             <th>Nama Siswa</th>
             <th>Jenis Kelamin</th>
             <th>Kelas</th>
             <th>Jurusan</th>
             <th>No HP</th>
-        </thead>
-        <tbody>
-            <?php foreach ($data as $value) : ?>
-                <tr>
-                    <td><?= $value['id_siswa']; ?></td>
-                    <td><?= $value['nama_siswa']; ?></td>
-                    <td><?= $value['jenis_kelamin']; ?></td>
-                    <td><?= $value['kelas']; ?></td>
-                    <td><?= $value['jurusan']; ?></td>
-                    <td><?= $value['no_hp']; ?></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+         </thead>
+         <tbody>
+            <?php $i = 1;
+            foreach ($data as $value) : ?>
+               <tr>
+                  <td><?= $i; ?></td>
+                  <td><?= $value['nis']; ?></td>
+                  <td><?= $value['nama_siswa']; ?></td>
+                  <td><?= $value['jenis_kelamin']; ?></td>
+                  <td><?= $value['kelas']; ?></td>
+                  <td><?= $value['jurusan']; ?></td>
+                  <td><?= $value['no_hp']; ?></td>
+               </tr>
+            <?php $i++;
+            endforeach; ?>
+         </tbody>
+      </table>
+   <?php else : ?>
+      <div class="row">
+         <div class="col">
+            <h4 class="text-center text-danger">Data tidak ditemukan</h4>
+         </div>
+      </div>
+   <?php endif; ?>
 </div>
