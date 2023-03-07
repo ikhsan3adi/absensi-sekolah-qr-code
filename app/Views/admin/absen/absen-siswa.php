3641ae9eda70e8dbc6e53c6f61c596b2dff2997b
Ikhsan S <93766033+x4nn@users.noreply.github.com>
2023-03-08 00:30:24 +0700

    Add refresh btn to list absen siswa
    Fix indent & bugs

--- app/Views/admin/absen/absen-siswa.php
+++ app/Views/admin/absen/absen-siswa.php
@@ -1,145 +1,144 @@
 <?= $this->extend('templates/admin_page_layout') ?>
 <?= $this->section('content') ?>
 <div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12 col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="pt-3 pl-3">
-                            <h4><b>Daftar Kelas</b></h4>
-                            <p>Silakan pilih kelas</p>
-                        </div>
+   <div class="container-fluid">
+      <div class="row">
+         <div class="col-lg-12 col-md-12">
+            <div class="card">
+               <div class="card-body">
+                  <div class="pt-3 pl-3">
+                     <h4><b>Daftar Kelas</b></h4>
+                     <p>Silakan pilih kelas</p>
+                  </div>
 
-                        <div class="card-body pt-1 px-3">
-                            <div class="row">
-                                <?php foreach ($data as $value) : ?>
-                                    <?php
-                                    $id_kelas = $value['id_kelas'];
-                                    $kelas =  $value['kelas'] . ' ' . $value['jurusan'];
-                                    ?>
-                                    <div class="col-md-3">
-                                        <button id="kelas-<?= $id_kelas; ?>" onclick="get_siswa(<?= $id_kelas; ?>, '<?= $kelas; ?>')" class="btn btn-primary w-100">
-                                            <?= $kelas; ?>
-                                        </button>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
+                  <div class="card-body pt-1 px-3">
+                     <div class="row">
+                        <?php foreach ($data as $value) : ?>
+                           <?php
+                           $id_kelas = $value['id_kelas'];
+                           $kelas =  $value['kelas'] . ' ' . $value['jurusan'];
+                           ?>
+                           <div class="col-md-3">
+                              <button id="kelas-<?= $id_kelas; ?>" onclick="get_siswa(<?= $id_kelas; ?>, '<?= $kelas; ?>')" class="btn btn-primary w-100">
+                                 <?= $kelas; ?>
+                              </button>
+                           </div>
+                        <?php endforeach; ?>
+                     </div>
+                  </div>
 
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="pt-3 pl-3 pb-2">
-                                    <h4><b>Tanggal</b></h4>
-                                    <input class="form-control" type="date" name="tangal" id="tanggal" value="<?= date('Y-m-d'); ?>" onchange="onDateChange(this.value)">
-                                </div>
-                            </div>
+                  <div class="row">
+                     <div class="col-md-3">
+                        <div class="pt-3 pl-3 pb-2">
+                           <h4><b>Tanggal</b></h4>
+                           <input class="form-control" type="date" name="tangal" id="tanggal" value="<?= date('Y-m-d'); ?>" onchange="onDateChange()">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-        <div class="card" id="dataSiswa">
-            <div class="card-body">
-                <div class="row justify-content-between">
-                    <div class="col-auto me-auto">
-                        <div class="pt-3 pl-3">
-                            <h4><b>Absen Siswa</b></h4>
-                            <p>Daftar siswa muncul disini</p>
-                        </div>
-                    </div>
-                </div>
+         </div>
+      </div>
+      <div class="card" id="dataSiswa">
+         <div class="card-body">
+            <div class="row justify-content-between">
+               <div class="col-auto me-auto">
+                  <div class="pt-3 pl-3">
+                     <h4><b>Absen Siswa</b></h4>
+                     <p>Daftar siswa muncul disini</p>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
-<script src="<?= base_url('public/assets/js/plugins/jquery/jquery-3.5.1.min.js') ?>"></script>
 <script>
-    var lastIdKelas;
-    var lastKelas;
+   var lastIdKelas;
+   var lastKelas;
 
-    function onDateChange(tanggal) {
-        if (lastIdKelas != null && lastKelas != null) get_siswa(lastIdKelas, lastKelas);
-    }
+   function onDateChange() {
+      if (lastIdKelas != null && lastKelas != null) get_siswa(lastIdKelas, lastKelas);
+   }
 
-    function get_siswa(id_kelas, kelas) {
-        var tanggal = $('#tanggal').val();
+   function get_siswa(id_kelas, kelas) {
+      var tanggal = $('#tanggal').val();
 
-        updateBtn(id_kelas);
+      updateBtn(id_kelas);
 
-        jQuery.ajax({
-            url: "<?= base_url('/admin/absen-siswa'); ?>",
-            type: 'post',
-            data: {
-                'kelas': kelas,
-                'id_kelas': id_kelas,
-                'tanggal': tanggal
-            },
-            success: function(response, status, xhr) {
-                // console.log(status);
-                $('#dataSiswa').html(response);
+      jQuery.ajax({
+         url: "<?= base_url('/admin/absen-siswa'); ?>",
+         type: 'post',
+         data: {
+            'kelas': kelas,
+            'id_kelas': id_kelas,
+            'tanggal': tanggal
+         },
+         success: function(response, status, xhr) {
+            // console.log(status);
+            $('#dataSiswa').html(response);
 
-                $('html, body').animate({
-                    scrollTop: $("#dataSiswa").offset().top
-                }, 500);
-            },
-            error: function(xhr, status, thrown) {
-                console.log(thrown);
-                $('#dataSiswa').html(thrown);
-            }
-        });
+            $('html, body').animate({
+               scrollTop: $("#dataSiswa").offset().top
+            }, 500);
+         },
+         error: function(xhr, status, thrown) {
+            console.log(thrown);
+            $('#dataSiswa').html(thrown);
+         }
+      });
 
-        lastIdKelas = id_kelas;
-        lastKelas = kelas;
-    }
+      lastIdKelas = id_kelas;
+      lastKelas = kelas;
+   }
 
-    function updateBtn(id_btn) {
-        for (let index = 1; index <= <?= count($data); ?>; index++) {
-            if (index != id_btn) {
-                $('#kelas-' + index).removeClass('btn-success');
-                $('#kelas-' + index).addClass('btn-primary');
+   function updateBtn(id_btn) {
+      for (let index = 1; index <= <?= count($data); ?>; index++) {
+         if (index != id_btn) {
+            $('#kelas-' + index).removeClass('btn-success');
+            $('#kelas-' + index).addClass('btn-primary');
+         } else {
+            $('#kelas-' + index).removeClass('btn-primary');
+            $('#kelas-' + index).addClass('btn-success');
+         }
+      }
+   }
+
+   function ubahKehadiran(formId) {
+      if (!confirm("Apakah yakin untuk mengubah kehadiran?")) {
+         return;
+      }
+
+      var tanggal = $('#tanggal').val();
+
+      var form = $('#formUbah' + formId).serializeArray();
+
+      form.push({
+         name: 'tanggal',
+         value: tanggal
+      });
+
+      console.log(form);
+
+      jQuery.ajax({
+         url: "<?= base_url('/admin/absen-siswa/edit'); ?>",
+         type: 'post',
+         data: form,
+         success: function(response, status, xhr) {
+            // console.log(status);
+
+            if (response['status']) {
+               get_siswa(lastIdKelas, lastKelas);
+               alert('Berhasil ubah kehadiran : ' + response['nama_siswa']);
             } else {
-                $('#kelas-' + index).removeClass('btn-primary');
-                $('#kelas-' + index).addClass('btn-success');
+               alert('Gagal ubah kehadiran : ' + response['nama_siswa']);
             }
-        }
-    }
-
-    function ubahKehadiran(formId) {
-        if (!confirm("Apakah yakin untuk mengubah kehadiran?")) {
-            return;
-        }
-
-        var tanggal = $('#tanggal').val();
-
-        var form = $('#formUbah' + formId).serializeArray();
-
-        form.push({
-            name: 'tanggal',
-            value: tanggal
-        });
-
-        console.log(form);
-
-        jQuery.ajax({
-            url: "<?= base_url('/admin/absen-siswa/edit'); ?>",
-            type: 'post',
-            data: form,
-            success: function(response, status, xhr) {
-                // console.log(status);
-
-                if (response['status']) {
-                    get_siswa(lastIdKelas, lastKelas);
-                    alert('Berhasil ubah kehadiran : ' + response['nama_siswa']);
-                } else {
-                    alert('Gagal ubah kehadiran : ' + response['nama_siswa']);
-                }
-            },
-            error: function(xhr, status, thrown) {
-                console.log(thrown);
-                alert('Gagal ubah kehadiran\n' + thrown);
-            }
-        });
-    }
+         },
+         error: function(xhr, status, thrown) {
+            console.log(thrown);
+            alert('Gagal ubah kehadiran\n' + thrown);
+         }
+      });
+   }
 </script>
 <?= $this->endSection() ?>
--- app/Views/admin/absen/list-absen-siswa.php
+++ app/Views/admin/absen/list-absen-siswa.php
@@ -1,10 +1,15 @@
 <div class="card-body">
-    <div class="row justify-content-between">
+    <div class="row">
         <div class="col-auto me-auto">
             <div class="pt-3 pl-3">
                 <h4><b>Absen Siswa</b></h4>
                 <p>Daftar siswa muncul disini</p>
             </div>
+        </div>
+        <div class="col">
+            <a href="#" class="btn btn-primary pl-3 mr-3 mt-3" onclick="kelas = onDateChange()" data-toggle="tab">
+                <i class="material-icons mr-2">refresh</i> Refresh
+            </a>
         </div>
         <div class="col-auto">
             <div class="px-4">
@@ -15,7 +20,7 @@
         </div>
     </div>
 
-    <div id="dataSiswa" class="card-body table-responsive">
+    <div id="dataSiswa" class="card-body table-responsive pb-5">
         <?php if (!empty($data)) : ?>
             <table class="table table-hover">
                 <thead class="text-success">
@@ -54,56 +59,54 @@
                                             <i class="material-icons">edit</i>
                                             Edit
                                         </button>
-                                        <ul class="dropdown-menu" aria-labelledby="<?= $value['nis']; ?>">
-                                            <li>
-                                                <h5 class="dropdown-header">Ubah kehadiran</h5>
-                                            </li>
+                                        <div class="dropdown-menu" aria-labelledby="<?= $value['nis']; ?>">
                                             <form id="formUbah<?= $value['id_siswa']; ?>">
-                                                <input type="hidden" name="id_siswa" value="<?= $value['id_siswa']; ?>">
-                                                <input type="hidden" name="id_kelas" value="<?= $value['id_kelas']; ?>">
-                                                <?php foreach ($listKehadiran as $value2) : ?>
-                                                    <li class="px-3">
-                                                        <div class="form-control">
-                                                            <?php $color = kehadiran($value2['id_kehadiran'])['color'] ?>
-                                                            <?php if ($value2['id_kehadiran'] == $id_kehadiran) : ?>
-                                                                <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>" checked>
-                                                                <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
-                                                                    <?= $value2['kehadiran']; ?>
-                                                                </label>
-                                                            <?php else : ?>
-                                                                <?php if ($value2['id_kehadiran'] == 1) : ?>
-                                                                    <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>">
-                                                                    <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
-                                                                        <?= $value2['kehadiran']; ?>
-                                                                    </label>
-                                                                <?php else : ?>
-                                                                    <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>">
-                                                                    <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
-                                                                        <?= $value2['kehadiran']; ?>
-                                                                    </label>
-                                                                <?php endif; ?>
-                                                            <?php endif; ?>
-                                                        </div>
-                                                    </li>
-                                                <?php endforeach; ?>
-                                                <li>
-                                                    <h5 class="dropdown-header pb-0">Keterangan</h5>
-                                                    <div class="p-3">
-                                                        <textarea name="keterangan"><?= $value['keterangan']; ?></textarea>
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <h5 class="dropdown-header">Ubah kehadiran</h5>
+                                                        <input type="hidden" name="id_siswa" value="<?= $value['id_siswa']; ?>">
+                                                        <input type="hidden" name="id_kelas" value="<?= $value['id_kelas']; ?>">
+                                                        <?php foreach ($listKehadiran as $value2) : ?>
+                                                            <div class="px-3">
+                                                                <div class="form-control">
+                                                                    <?php $color = kehadiran($value2['id_kehadiran'])['color'] ?>
+                                                                    <?php if ($value2['id_kehadiran'] == $id_kehadiran) : ?>
+                                                                        <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>" checked>
+                                                                        <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
+                                                                            <?= $value2['kehadiran']; ?>
+                                                                        </label>
+                                                                    <?php elseif ($value2['id_kehadiran'] == 1) : ?>
+                                                                        <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>">
+                                                                        <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
+                                                                            <?= $value2['kehadiran']; ?>
+                                                                        </label>
+                                                                    <?php else : ?>
+                                                                        <input type="radio" name="id_kehadiran" id="<?= $value2['id_kehadiran']; ?>" value="<?= $value2['id_kehadiran']; ?>">
+                                                                        <label class="form-check-label text-<?= $color; ?>" for="<?= $value2['id_kehadiran']; ?>">
+                                                                            <?= $value2['kehadiran']; ?>
+                                                                        </label>
+                                                                    <?php endif; ?>
+                                                                </div>
+                                                            </div>
+                                                        <?php endforeach; ?>
                                                     </div>
-                                                </li>
-                                            </form>
-                                            <li>
-                                                <div class="px-3 pb-3">
-                                                    <button id="tombolUbah<?= $value['id_siswa']; ?>" class="btn btn-success w-100">Ubah</button>
+                                                    <div class="col">
+                                                        <h5 class="dropdown-header pb-0 pl-0">Keterangan</h5>
+                                                        <div class="pr-3 py-3">
+                                                            <textarea name="keterangan"><?= trim($value['keterangan']); ?></textarea>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <script>
-                                                    $('#tombolUbah' + <?= $value['id_siswa']; ?>).click(function(e) {
-                                                        ubahKehadiran(<?= $value['id_siswa']; ?>);
-                                                    });
-                                                </script>
-                                            </li>
-                                        </ul>
+                                            </form>
+                                            <div class="p-3">
+                                                <button id="tombolUbah<?= $value['id_siswa']; ?>" class="btn btn-success w-100">Ubah</button>
+                                            </div>
+                                            <script>
+                                                $('#tombolUbah' + <?= $value['id_siswa']; ?>).click(function(e) {
+                                                    ubahKehadiran(<?= $value['id_siswa']; ?>);
+                                                });
+                                            </script>
+                                        </div>
                                     </div>
                                 <?php else : ?>
                                     <button class="btn btn-disabled p-2">No Action</button>
