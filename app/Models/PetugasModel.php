f5bd231af86faf77be557b0776a247f62fc1456a
RPL Cinta Niaga <93766033+x4nn@users.noreply.github.com>
2023-03-13 12:43:19 +0700

    Update PetugasModel.php
    save petugas
    getpetugas

--- app/Models/PetugasModel.php
+++ app/Models/PetugasModel.php
@@ -6,17 +6,36 @@ use CodeIgniter\Model;
 
 class PetugasModel extends Model
 {
-   // protected function initialize()
-   // {
-   //     $this->allowedFields[] = [];
-   // }
+   protected function initialize()
+   {
+      $this->allowedFields = [
+         'email',
+         'username',
+         'superadmin'
+      ];
+   }
 
    protected $table = 'tb_petugas';
 
-   protected $primaryKey = 'id_petugas';
+   protected $primaryKey = 'id';
 
    public function getAllPetugas()
    {
       return $this->findAll();
+   }
+
+   public function getPetugasById($id)
+   {
+      return $this->where([$this->primaryKey => $id])->first();
+   }
+
+   public function savePetugas($idPetugas, $email, $username, $role)
+   {
+      return $this->save([
+         $this->primaryKey => $idPetugas,
+         'email' => $email,
+         'username' => $username,
+         'superadmin' => $role ?? '0',
+      ]);
    }
 }
