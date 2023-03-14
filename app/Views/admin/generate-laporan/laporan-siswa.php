db916e03b43193abbef85e392374d7e63d8b8ee6
RPL Cinta Niaga <93766033+x4nn@users.noreply.github.com>
2023-03-14 08:42:42 +0700

    Add logo to generated laporan

--- app/Views/admin/generate-laporan/laporan-siswa.php
+++ app/Views/admin/generate-laporan/laporan-siswa.php
@@ -1,8 +1,19 @@
 <?= $this->extend('templates/laporan') ?>
 
 <?= $this->section('content') ?>
-<h2 align="center">DAFTAR HADIR SISWA</h2>
-<h4 align="center">TAHUN PELAJARAN 2022/2023</h4>
+<table>
+   <tr>
+      <td><img src="<?= base_url('public/assets/img/icbcn_logo.jpg'); ?>" width="100px"></td>
+      <td width="100%">
+         <h2 align="center">DAFTAR HADIR SISWA</h2>
+         <h4 align="center">SMK ICB CINTA NIAGA</h4>
+         <h4 align="center">TAHUN PELAJARAN 2022/2023</h4>
+      </td>
+      <td>
+         <div style="width:100px"></div>
+      </td>
+   </tr>
+</table>
 <span>Bulan : <?= $bulan; ?></span>
 <span style="position: absolute;right: 0;">Kelas : <?= "{$kelas['kelas']} {$kelas['jurusan']}"; ?></span>
 <table align="center" border="1">
@@ -20,7 +31,7 @@
    </thead>
    <tr>
       <th id="rowSpan3" align="center">No</th>
-      <th id="rowSpan3" width="250px">Nama</th>
+      <th id="rowSpan3" width="1000px">Nama</th>
       <?php foreach ($tanggal as $value) : ?>
          <th align="center"><?= $value->format('d'); ?></th>
       <?php endforeach; ?>
