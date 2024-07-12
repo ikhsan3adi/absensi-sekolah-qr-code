b0aba45be59d1ba8cf3fd200a14916bcf1b0923f
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-07-12 15:34:26 +0700

    feat: Add single qr download
    download per siswa/guru

--- app/Views/admin/data/list-data-guru.php
+++ app/Views/admin/data/list-data-guru.php
@@ -8,7 +8,7 @@
             <th><b>Jenis Kelamin</b></th>
             <th><b>No HP</b></th>
             <th><b>Alamat</b></th>
-            <th><b>Aksi</b></th>
+            <th width="1%"><b>Aksi</b></th>
          </thead>
          <tbody>
             <?php $i = 1;
@@ -21,18 +21,24 @@
                   <td><?= $value['no_hp']; ?></td>
                   <td><?= $value['alamat']; ?></td>
                   <td>
-                     <a href="<?= base_url('admin/guru/edit/' . $value['id_guru']); ?>" type="button" class="btn btn-success p-2" id="<?= $value['nuptk']; ?>">
-                        <i class="material-icons">edit</i>
-                        Edit
-                     </a>
-                     <form action="<?= base_url('admin/guru/delete/' . $value['id_guru']); ?>" method="post" class="d-inline">
-                        <?= csrf_field(); ?>
-                        <input type="hidden" name="_method" value="DELETE">
-                        <button onclick="return confirm('Konfirmasi untuk menghapus data');" type="submit" class="btn btn-danger p-2" id="<?= $value['nuptk']; ?>">
-                           <i class="material-icons">delete_forever</i>
-                           Delete
-                        </button>
-                     </form>
+                     <div class="d-flex justify-content-center">
+                        <a href="<?= base_url('admin/guru/edit/' . $value['id_guru']); ?>" class="btn btn-success p-2" id="<?= $value['nuptk']; ?>">
+                           <i class="material-icons">edit</i>
+                           Edit
+                        </a>
+                        <form action="<?= base_url('admin/guru/delete/' . $value['id_guru']); ?>" method="post" class="d-inline">
+                           <?= csrf_field(); ?>
+                           <input type="hidden" name="_method" value="DELETE">
+                           <button onclick="return confirm('Konfirmasi untuk menghapus data');" type="submit" class="btn btn-danger p-2" id="<?= $value['nuptk']; ?>">
+                              <i class="material-icons">delete_forever</i>
+                              Delete
+                           </button>
+                        </form>
+                        <a href="<?= base_url('admin/qr/guru/' . $value['id_guru'] . '/download'); ?>" class="btn btn-success p-2">
+                           <i class="material-icons">qr_code</i>
+                           Download QR
+                        </a>
+                     </div>
                   </td>
                </tr>
             <?php $i++;
