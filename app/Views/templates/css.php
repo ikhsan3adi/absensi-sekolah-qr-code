708ec4de5ca513c071c5d1e80e3852bc29cfa3af
Reactmore <reactmorecom@gmail.com>
2024-07-24 19:10:55 +0700

    added custom styling css

--- app/Views/templates/css.php
+++ app/Views/templates/css.php
@@ -1,6 +1,7 @@
 <!-- CSS Files -->
 <link href="<?= base_url('assets/fonts/fonts.css?v=1.0.0'); ?>" rel="stylesheet" />
 <link href="<?= base_url('assets/css/material-dashboard.css'); ?>" rel="stylesheet" />
+<link href="<?= base_url('assets/css/style.css?v=1.0.0'); ?>" rel="stylesheet" />
 
 <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/img/apple-icon.png'); ?>">
 <link rel="icon" type="image/png" href="<?= base_url('assets/img/favicon.png'); ?>">
