aea7f3f2a814366b3c15f78db952f73cb6957cd5
hex4coder <the.programmer.luyo@gmail.com>
2025-02-14 09:50:38 +0800

    Update role untuk petugas

--- app/Views/admin/absen/list-absen-guru.php
+++ app/Views/admin/absen/list-absen-guru.php
@@ -31,7 +31,7 @@
                   <td><b><?= $value['jam_keluar'] ?? '-'; ?></b></td>
                   <td><?= $value['keterangan'] ?? '-'; ?></td>
                   <td>
-                     <?php if (!$lewat && user()->toArray()['is_superadmin'] == 1) : ?>
+                     <?php if (!$lewat && (user()->toArray()['is_superadmin'] == 1 || user()->toArray()['is_superadmin'] == 3) && (!(str_contains(strtolower($value['nama_guru']), "yuyun") || str_contains(strtolower($value['nama_guru']), "ahmad") || str_contains(strtolower($value['nama_guru']), "faisal") || str_contains(strtolower($value['nama_guru']), "sarmin")))) : ?>
                         <button data-toggle="modal" data-target="#ubahModal" onclick="getDataKehadiran(<?= $value['id_presensi'] ?? '-1'; ?>, <?= $value['id_guru']; ?>)" class="btn btn-info p-2" id="<?= $value['id_guru']; ?>">
                            <i class="material-icons">edit</i>
                            Edit
