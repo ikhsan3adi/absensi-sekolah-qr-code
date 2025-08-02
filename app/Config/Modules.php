2fcedbd0291c63c62bc2f74b4cb3b1521c5f5cc7
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-08-02 20:20:01 +0700

    feat: upgrade framework to v4.6.2
    Upgrades the CodeIgniter framework to version 4.6.2.
    This includes various bug fixes, performance improvements, and vulnerability fixes.

--- app/Config/Modules.php
+++ app/Config/Modules.php
@@ -4,6 +4,12 @@ namespace Config;
 
 use CodeIgniter\Modules\Modules as BaseModules;
 
+/**
+ * Modules Configuration.
+ *
+ * NOTE: This class is required prior to Autoloader instantiation,
+ *       and does not extend BaseConfig.
+ */
 class Modules extends BaseModules
 {
     /**
@@ -50,7 +56,7 @@ class Modules extends BaseModules
      *       ],
      *   ]
      *
-     * @var array
+     * @var array{only?: list<string>, exclude?: list<string>}
      */
     public $composerPackages = [];
 
@@ -64,7 +70,7 @@ class Modules extends BaseModules
      *
      * If it is not listed, only the base application elements will be used.
      *
-     * @var string[]
+     * @var list<string>
      */
     public $aliases = [
         'events',
